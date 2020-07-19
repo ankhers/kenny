18ae1079efbdc2227790cf4aed708afadd02a81b
Justin Wood <me@ankhers.dev>
2020-07-19 15:24:01 -0400

    Add basic auth to prevent malicious usage.

--- lib/kenny_web/router.ex
+++ lib/kenny_web/router.ex
@@ -1,12 +1,15 @@
 defmodule KennyWeb.Router do
   use KennyWeb, :router
 
+  import Plug.BasicAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :basic_auth, Application.get_env(:kenny, :basic_auth)
   end
 
   pipeline :api do
